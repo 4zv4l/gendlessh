267e6e1cfe4e747c3efa51890ab758bf33aa21a1
4zv4l <4zv4l@protonmail.com>
2026-09-16 18:14:48 +0800

    fix: seconds float format

--- endlessh.go
+++ endlessh.go
@@ -43,7 +43,7 @@ func main() {
 func tarpit(conn net.Conn, delay uint, grp <-chan struct{}) {
 	start := time.Now()
 	defer func() {
-		slog.Info("Victim escaped", "addr", conn.RemoteAddr(), "duration", fmt.Sprintf("%d", time.Since(start).Seconds()))
+		slog.Info("Victim escaped", "addr", conn.RemoteAddr(), "duration", fmt.Sprintf("%.0f", time.Since(start).Seconds()))
 		conn.Close()
 		<-grp
 	}()
